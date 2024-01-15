f0261ef0911cba93a990763786617c877f2bf378
kimjinwoo <79128016+rlawlsdn263@users.noreply.github.com>
2024-01-15 11:10:05 +0900

    [level Lv.0] Title: 문자열 섞기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/Lv.0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,3 +1,9 @@
 def solution(str1, str2):
-    result = "".join(a + b for a, b in zip(str1, str2))
-    return result
+    answer = ''
+
+    for i in range(len(str1)):
+        answer += str1[i] + str2[i]
+
+    return answer
+
+print(solution("aaaaa", "bbbbb"))
