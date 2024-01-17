cc9e67254e6352ca44b9a91906c3ae560467cf29
kimjinwoo <79128016+rlawlsdn263@users.noreply.github.com>
2024-01-17 11:34:33 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.86 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,12 +1,12 @@
 def solution(n, control):
-    # control 문자열의 각 문자에 대해 반복
-    for char in control:
-        if char == 'w':
-            n += 1  # 'w'이면 n을 1 증가
-        elif char == 's':
-            n -= 1  # 's'이면 n을 1 감소
-        elif char == 'd':
-            n += 10  # 'd'이면 n을 10 증가
-        elif char == 'a':
-            n -= 10  # 'a'이면 n을 10 감소
-    return n  # 최종적으로 변경된 n 값을 반환
+    for c in control:
+        if c == "w":
+            n += 1
+        elif c == "s":
+            n -= 1
+        elif c == "d":
+            n += 10
+        elif c == "a":
+            n -= 10
+    
+    return n
