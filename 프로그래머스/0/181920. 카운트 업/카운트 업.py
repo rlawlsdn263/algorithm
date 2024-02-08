08a8a48cb07a7bb5a1f22bfa0239d3d2a72f0eca
kimjinwoo <79128016+rlawlsdn263@users.noreply.github.com>
2024-02-08 23:23:56 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.py
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -1,2 +1,7 @@
 def solution(start_num, end_num):
-    return list(range(start_num, end_num + 1))
+    answer = []
+    
+    for i in range(start_num, end_num + 1):
+        answer.append(i)
+    
+    return answer
