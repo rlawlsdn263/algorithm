b21f37ad97e2fff65d7f49a823a2bb46aa7156c2
kimjinwoo <79128016+rlawlsdn263@users.noreply.github.com>
2024-02-10 22:45:33 +0900

    [level 0] Title: 글자 지우기, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -2,7 +2,7 @@ def solution(my_string, indices):
     new_str = ""
 
     for i in range(len(my_string)):
-        if i not in sorted(indices):
+        if i not in indices:
             new_str += my_string[i]
             
     return new_str
