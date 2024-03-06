90b63f154d76abbc0421af11d745b6a6d929fe61
kimjinwoo <79128016+rlawlsdn263@users.noreply.github.com>
2024-03-06 14:58:46 +0900

    [level 0] Title: 문자열 묶기, Time: 8.33 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,17 +1,4 @@
 def solution(strArr):
-    answer = 0
-    length_dict = {}
-
-    for str in strArr:
-        str_length = len(str)
-
-        if str_length not in length_dict:
-            length_dict[str_length] = []
-
-        length_dict[str_length].append(str)
-
-    for dict in length_dict:
-        if len(length_dict[dict]) > answer:
-            answer = len(length_dict[dict])
-    
-    return answer
+    a=[0]*31
+    for x in strArr: a[len(x)]+=1
+    return max(a)
