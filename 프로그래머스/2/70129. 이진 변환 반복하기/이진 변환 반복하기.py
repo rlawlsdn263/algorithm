e4ce3e4413082125f739491eecac751abf0e4205
kimjinwoo <79128016+rlawlsdn263@users.noreply.github.com>
2024-03-18 15:09:31 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 7.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,10 +1,19 @@
 def solution(s):
-    change, zero = 0, 0
-
-    while s != '1':
+    change = 0
+    zero = 0 # 제거된 0의 개수를 넣어야 한다
+    
+    while s != "1":
+        print(s)
         change += 1
-        num = s.count('1')
-        zero += len(s) - num
-        s = bin(num)[2:]
+        
+        for n in s: # 0의 값 구하기
+            if n == '0':
+                zero += 1
+
+        new = s.replace("0", "") # 0 제거
+
+        # 10진수를 2진수로 변경하고 그 값을 counting한다
+        s = bin(len(new))[2:]
+        
 
     return [change, zero]
