51511bd3b22675b91ed975518b28941dafc96b8b
kimjinwoo <79128016+rlawlsdn263@users.noreply.github.com>
2024-03-26 10:31:14 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,14 +1,18 @@
 def solution(s):
-    array = []
+    answer = ''
+    # 단어별로 처리하기 위해 인덱스를 사용합니다.
+    index = 0
+    for char in s:
+        # 공백을 만날 경우 인덱스를 초기화합니다.
+        if char == ' ':
+            answer += char
+            index = 0
+        else:
+            # 짝수 인덱스의 문자는 대문자로, 홀수 인덱스의 문자는 소문자로 변환합니다.
+            if index % 2 == 0:
+                answer += char.upper()
+            else:
+                answer += char.lower()
+            index += 1
 
-    splitArr = s.split(" ")
-    for split in splitArr:
-        word = ""
-        for i, v in enumerate(split):
-            if i % 2 == 0:
-                word += v.upper()
-            else: 
-                word += v.lower()
-        array.append(word)
-
-    return " ".join(array)
+    return answer
