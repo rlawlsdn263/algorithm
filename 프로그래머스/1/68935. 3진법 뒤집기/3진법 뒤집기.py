72105555d609978f7aa457474c07dccbf9daa86c
kimjinwoo <79128016+rlawlsdn263@users.noreply.github.com>
2024-03-28 00:51:29 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,14 +1,9 @@
-def decimal_to_ternary(n):
+def solution(n):
     if n == 0:
         return '0'
-    ternary = ''
+    ternary = ""
     while n > 0:
         ternary = str(n % 3) + ternary
         n = n // 3
-    return ternary
 
-def solution(n):
-    ternary = decimal_to_ternary(n)
-    reversed_ternary = ternary[::-1]
-
-    return int(reversed_ternary, 3)
+    return int(ternary[::-1], 3)
