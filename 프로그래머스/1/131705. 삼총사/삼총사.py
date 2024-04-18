2b0467e6185a311f68d84306994283b23c3cdfe5
kimjinwoo <79128016+rlawlsdn263@users.noreply.github.com>
2024-04-18 17:06:14 +0900

    [level 1] Title: 삼총사, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,8 +1,11 @@
 def solution(number):
-    count = 0
+    answer = 0
+
     for i in range(len(number)):
-        for j in range(i+1, len(number)):
-            for k in range(j+1, len(number)):
-                if int(number[i]) + int(number[j]) + int(number[k]) == 0:
-                    count += 1
-    return count
+        for j in range(i, len(number)):
+            for k in range(j, len(number)):
+                if number[i] + number[j] + number[k] == 0 and i != j != k:
+                    print(number[i], number[j], number[k])
+                    answer += 1
+
+    return answer
