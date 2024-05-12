836b3d73cf2840f0565dcb8d7a70347c4d3fe9bf
kimjinwoo <79128016+rlawlsdn263@users.noreply.github.com>
2024-05-12 23:36:46 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,10 +1,11 @@
 def solution(k, m, score):
-    score.sort(reverse=True)  # 점수를 내림차순으로 정렬
-    max_profit = 0
+    answer = 0
     
-    for i in range(0, len(score), m):
-        if i + m <= len(score):
-            min_score = score[i + m - 1]  # 상자에 담긴 사과 중 최저 점수
-            max_profit += min_score * m
+    reversed = sorted(score, reverse=True) # [3, 3, 2, 2, 1, 1, 1]
     
-    return max_profit
+    for i in range(0, len(reversed), m): # 0 4
+      if i + m <= len(reversed): # 0 + 4 <= 7, 초과하는 사과는 버려진다
+        min_price = reversed[i + m - 1] * m # 최저값은 배열의 마지막 요소, 고로 최저값은 마지막 요소 * m
+        answer += min_price
+        
+    return answer
