621f93693c9402cdb234d747a9403cff2d1596b4
kimjinwoo <79128016+rlawlsdn263@users.noreply.github.com>
2024-05-13 22:26:07 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,11 +1,11 @@
 def solution(k, m, score):
     answer = 0
     
-    reversed = sorted(score, reverse=True) # [3, 3, 2, 2, 1, 1, 1]
+    reverse = sorted(score, reverse=True)
     
-    for i in range(0, len(reversed), m): # 0 4
-      if i + m <= len(reversed): # 0 + 4 <= 7, 초과하는 사과는 버려진다
-        min_price = reversed[i + m - 1] * m # 최저값은 배열의 마지막 요소, 고로 최저값은 마지막 요소 * m
-        answer += min_price
-        
+    for i in range(0, len(score), m):
+      if i + m <= len(score):
+        min_score = reverse[i + m - 1]
+        answer += min_score * m
+      
     return answer
