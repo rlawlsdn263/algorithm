4a7de76d9ed585c4cd9b2801bef748da8124e736
kimjinwoo <79128016+rlawlsdn263@users.noreply.github.com>
2024-06-10 09:26:25 +0900

    Time: 48 ms (7.62%), Space: 16.6 MB (20.74%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,25 +1,5 @@
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        # Two pointers for nums1 and nums2
-        p1, p2 = m - 1, n - 1
-        # Pointer for the end of merged array
-        p = m + n - 1
-
-        # Compare from the end and merge nums2 into nums1
-        while p1 >= 0 and p2 >= 0:
-            if nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
-            else:
-                nums1[p] = nums2[p2]
-                p2 -= 1
-            p -= 1
-
-        # If there are remaining elements in nums2, copy them
-        while p2 >= 0:
-            nums1[p] = nums2[p2]
-            p -= 1
-            p2 -= 1
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+      for j in range(n):
+          nums1[m+j] = nums2[j]
+      nums1.sort()
